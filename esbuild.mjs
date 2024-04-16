7ee2419bc4fd622f5195c6e6cd7747d3b2b1a3ba
Slee Woo <mail@sleewoo.com>
2024-04-16 10:22:35 +0300

    refined repo builder

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,23 +1,19 @@
 import nopt from "nopt";
 import { build } from "esbuild";
 
+import config from "./esbuild.json" with { type: "json" };
+
 const { argv, ...opts } = nopt({});
 
 for (const entryPoint of argv.remain) {
   await build({
-    bundle: true,
     entryPoints: [`src/${entryPoint}.ts`],
-    // overridable options
+    outfile: `pkg/${entryPoint}.mjs`,
+    loader: { ".hbs": "text" },
+    ...config,
     ...Object.entries(opts).reduce((a, [k, v]) => {
       a[k.replace(/(\w)\-(\w)/g, (_m, a, b) => a + b.toUpperCase())] = v;
       return a;
     }, {}),
-    outfile: `pkg/${entryPoint}.mjs`,
-    platform: "node",
-    target: "node20",
-    format: "esm",
-    loader: { ".hbs": "text" },
-    packages: "external",
-    sourcemap: "inline",
   });
 }
