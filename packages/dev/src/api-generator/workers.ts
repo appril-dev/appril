e9d33124bbb61bd9db6ab4bdb6693635655eb97c
Slee Woo <mail@sleewoo.com>
2024-05-10 07:01:32 +0300

    dev - export base use middleware

--- packages/dev/src/api-generator/workers.ts
+++ packages/dev/src/api-generator/workers.ts
@@ -92,6 +92,7 @@ async function generateRouteFiles({
       {
         template: baseTpl,
         context: {
+          sourceFolder,
           apiMethods: Object.keys(APIMethods),
           route,
         },
