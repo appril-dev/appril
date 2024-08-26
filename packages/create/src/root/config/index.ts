9f4043749b6660f0336a6e8db234820099664934
Slee Woo <mail@sleewoo.com>
2024-08-26 21:19:13 +0300

    create: chore - remove DEBUG config key

--- packages/create/src/root/config/index.ts
+++ packages/create/src/root/config/index.ts
@@ -1,11 +1,9 @@
 export const hostname = process.env.HOSTNAME ?? "";
-export const DEBUG = Boolean(process.env.DEBUG);
 export const NODE_ENV = process.env.NODE_ENV ?? "development";
 export const DEV = NODE_ENV === "development";
 
 export default {
   hostname,
-  DEBUG,
   NODE_ENV,
   DEV,
 };
