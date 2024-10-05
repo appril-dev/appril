e4d82f8cedde21c4de55de1471c2b8a29be08c1b
Slee Woo <mail@sleewoo.com>
2024-10-05 19:29:47 +0300

    run api router in exclusive mode

--- packages/create/src/src/api/router.ts
+++ packages/create/src/src/api/router.ts
@@ -1,5 +1,8 @@
 import Router from "@koa/router";
 
-const router = new Router();
+const router = new Router({
+  // highly important! only run last matched route when there are multiple matches
+  exclusive: true,
+});
 
 export default router;
